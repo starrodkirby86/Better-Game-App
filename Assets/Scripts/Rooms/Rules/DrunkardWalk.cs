c736e30b3b19b9bb1eaebaefc10500f1ba68ac88
Watson Tungjunyatham <starrodkirby86@mail.fresnostate.edu>
2016-02-28 09:33:01 -0800

    Added rotation functions
    UUDDLRLRBA start

--- Assets/Scripts/Rooms/Rules/DrunkardWalk.cs
+++ Assets/Scripts/Rooms/Rules/DrunkardWalk.cs
@@ -3,7 +3,13 @@
  * The drunkard walk algorithm is a dungeon-method algorithm that creates
  * a natural, staggered, linear movement of a corridor.
  * 
+ * A drunken approach takes a random cell, chooses a random legal
+ * cardinal direction, move in that direction and repeat until the
+ * quota finishes.
  * 
+ * However, to aim for a stronger overall path, heuristics should
+ * be given to gravitate towards the center. Furthermore, depending
+ * on the scale, the spaces should be mangnified a bit.
  *
  */
 
@@ -17,7 +23,18 @@ public class DrunkardWalk : BaseRuleset {
 		col = c;
 		map = new Tile[row,col];
 	}
-	
+
+	public void explodeSpace(Tile[,] map, Coord state, int m, int r, int c) {
+		// Carves additional spaces surrounding the block.
+		// The magnitude value m how far the cursor should carve.
+		// What doesn't get carved:
+		// 	Illegal OOB spots.
+		//	Border spaces
+		for(int i = 0; i < m; i++) {
+			// Getting there. :)
+		}
+	}
+
 	public override void generateMap() {
 	
 	}
--- Assets/Scripts/Types/Grid.cs
+++ Assets/Scripts/Types/Grid.cs
@@ -64,4 +64,49 @@ public class Coord {
 		         successor.y >= c
 		        );
 	}
+
+	public Direction rotate180(Direction d) {
+		switch(d) {
+		case Direction.North:
+			return Direction.South;
+		case Direction.West:
+			return Direction.East;
+		case Direction.South:
+			return Direction.North;
+		case Direction.East:
+			return Direction.West;
+		default:
+			return d;
+		}
+	}
+
+	public Direction rotate90Right(Direction d) {
+		switch(d) {
+		case Direction.North:
+			return Direction.East;
+		case Direction.West:
+			return Direction.North;
+		case Direction.South:
+			return Direction.West;
+		case Direction.East:
+			return Direction.South;
+		default:
+			return d;
+		}
+	}
+
+	public Direction rotate90Left(Direction d) {
+		switch(d) {
+		case Direction.North:
+			return Direction.West;
+		case Direction.West:
+			return Direction.South;
+		case Direction.South:
+			return Direction.East;
+		case Direction.East:
+			return Direction.North;
+		default:
+			return d;
+		}
+	}
 }
