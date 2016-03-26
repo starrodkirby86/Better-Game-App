02980a7383182df5852a216b2b5ed0fe39896488
Watson Tungjunyatham <starrodkirby86@mail.fresnostate.edu>
2016-03-26 15:45:41 -0700

    Set up for Room Utility Functions
    thanks fum

---
+++ Assets/Scripts/Rooms/RoomUtilityFunctions.cs
@@ -0,0 +1,29 @@
+/**
+ * RoomUtilityFunctions.cs
+ * 
+ * Sometimes you need a set of generic functions to help you get along your way.
+ * This is what this script does. It'll host plenty of generic room generation
+ * functions that can be helpful in providing the coder useful information
+ * or to make generating something easier.
+ */
+
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+/**
+ * Generic functions that help make adding certain
+ * tiles when generating algorithms.
+ */
+public class TileFunctions {
+
+}
+
+/**
+ * Fnuctions that help verify aspects of a map to 
+ * help ensure that the map made is a "good" one.
+ */
+public class MapValidationFunctions {
+
+}
