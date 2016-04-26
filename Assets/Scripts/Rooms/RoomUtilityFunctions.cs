3effff960923677473375af6e2b295e05612b114
andysokk <andysokk@mail.fresnostate.edu>
2016-04-25 23:35:39 -0700

    Floodfill base algo
    Floodfill base algo added

--- Assets/Scripts/Rooms/RoomUtilityFunctions.cs
+++ Assets/Scripts/Rooms/RoomUtilityFunctions.cs
@@ -22,11 +22,44 @@ public class TileFunctions {
 }
 
 /**
- * Fnuctions that help verify aspects of a map to 
+ * Functions that help verify aspects of a map to 
  * help ensure that the map made is a "good" one.
  * -> DFS
  * -> Floodfill a particular point and return the area
  */
 public class MapValidationFunctions {
+	Boolean clearable = false;
 
+public void FloodFillCheck(Tile[,] map, int x, int y, Tile[,] oldChar, Tile[,] newChar) {
+	// The recursive algorithm. Starting at x and y, changes any adjacent
+	// characters that match oldChar to newChar.
+		int mapWidth = map.GetLength(0);
+		int mapHeight = map.GetLength(1);
+
+		if (oldChar == null) {
+			oldChar = map[x][y];
+		}
+
+		if (map[x][y] != oldChar)
+			// Base case. If the current x, y character is not the oldChar,
+			// then do nothing.
+			return
+
+				// Change the character at world[x][y] to newChar
+				map[x][y] = newChar;
+
+				// Recursive calls. Make a recursive call as long as we are not on the
+				// boundary (which would cause an Index Error.)
+		if (x > 0) // left
+			FloodFillCheck(map, x-1, y, oldChar, newChar);
+
+		if (y > 0) // up
+			FloodFillCheck(map, x, y-1, oldChar, newChar);
+
+		if (x < mapWidth-1) // right
+			FloodFillCheck(map, x+1, y, oldChar, newChar);
+
+		if (y < mapHeight-1) // down
+			FloodFillCheck(map, x, y+1, oldChar, newChar);
+						}
 }
